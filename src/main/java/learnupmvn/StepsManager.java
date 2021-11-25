475ac8d307913a931d523befc6dff8bc3335693f
Daniil Kornilov <kristinaultra@mail.ru>
2021-11-25 04:17:36 +0300

    Add Step manager

---
+++ src/main/java/learnupmvn/StepsManager.java
@@ -0,0 +1,39 @@
+package learnupmvn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class StepsManager {
+    public int maxSteps;
+
+    private Map<Integer, Integer> stat = new HashMap<>();
+
+    public Map<Integer, Integer> getStat() {
+        return stat;
+    }
+
+    public int add(int day, int steps) {
+        if (stat.containsKey(day)){
+            stat.put(day, stat.get(day) + steps);
+        }
+        else{
+            stat.put(day, steps);
+        }
+
+        setMaxSteps();
+        return maxSteps - stat.get(day) + 1;
+    }
+
+    public int setMaxSteps(){
+        Set<Integer> days = stat.keySet();
+        Integer[] arrDays = days.toArray(new Integer[days.size()]);
+        for(int i = 0; i < arrDays.length; i++){
+            if (stat.get(arrDays[i]) > maxSteps){
+                maxSteps = stat.get(arrDays[i]);
+            }
+        }
+        return maxSteps;
+    }
+}
